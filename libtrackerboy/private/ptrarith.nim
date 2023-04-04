22bf97b4cb0ff27131bddd26c17b96ebf5a79956
stoneface <44489054+stoneface86@users.noreply.github.com>
2023-04-04 12:34:10 -0400

    fix deprecation warning

--- libtrackerboy/private/ptrarith.nim
+++ libtrackerboy/private/ptrarith.nim
@@ -13,13 +13,13 @@ template ptrArith*(body: untyped) =
   # in a body ;)
 
   template `+`[T](p: ptr T, off: int): ptr T {.used.} =
-    cast[ptr type(p[])](cast[ByteAddress](p) +% off * sizeof(p[]))
+    cast[ptr type(p[])](cast[int](p) +% off * sizeof(p[]))
 
   template `+=`[T](p: ptr T, off: int) {.used.} =
     p = p + off
   
   template `-`[T](p: ptr T, off: int): ptr T {.used.} =
-    cast[ptr type(p[])](cast[ByteAddress](p) -% off * sizeof(p[]))
+    cast[ptr type(p[])](cast[int](p) -% off * sizeof(p[]))
   
   template `-=`[T](p: ptr T, off: int) {.used.} =
     p = p - off
